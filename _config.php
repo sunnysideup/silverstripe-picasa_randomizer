8b0207ebf81e9aa801f2370a7d027089db5344d8
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2014-04-04 21:01:54 +0000

    UPGRADING 3.1
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/picasa_randomizer/trunk@5539 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- _config.php
+++ _config.php
@@ -1,13 +1,2 @@
 <?php
 
-
-/**
-*@author Nicolaas [at] sunnysideup.co.nz
-*
-**/
-Director::addRules(50, array(
-	'randompicassaimage//$Action/$ID/$Batch' => 'PicasaRandomImage_Controller'
-));
-//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-//===================---------------- START picasarandomizer MODULE ----------------===================
-//===================---------------- END picasarandomizer MODULE ----------------===================
